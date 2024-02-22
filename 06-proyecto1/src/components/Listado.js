7da5b7e12b0cf6b167440c3d66dee3379278a4a2
Damaso Garcia Bouza <damasogb@gmail.com>
2024-02-22 14:36:27 +0100

    Listado peliculas

--- 06-proyecto1/src/components/Listado.js
+++ 06-proyecto1/src/components/Listado.js
@@ -1,32 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Listado = () => {
+
+    const [listState, setListState] = useState([]);
+
+    useEffect(() => {
+        console.log("Componente del listado de peliculas cargado!");
+        getMovies();
+    }, []);
+
+    const getMovies = () => {
+        let movies = JSON.parse(localStorage.getItem("movies"));
+        console.log(movies);
+        setListState(movies);
+    }
+
     return (
         <>
-            <article className="peli-item">
-                <h3 className="title">Batman</h3>
-                <p className="description">A bat hero</p>
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-            <article className="peli-item">
-                <h3 className="title">Batman</h3>
-                <p className="description">A bat hero</p>
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-            <article className="peli-item">
-                <h3 className="title">Batman</h3>
-                <p className="description">A bat hero</p>
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-            <article className="peli-item">
-                <h3 className="title">Batman</h3>
-                <p className="description">A bat hero</p>
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
+            {listState != null ?
+                listState.map(movie => {
+
+                    return (
+                        <article key={movie.id} className="peli-item">
+                            <h3 className="title">{movie.title}</h3>
+                            <p className="description">{movie.description}</p>
+                            <button className="edit">Editar</button>
+                            <button className="delete">Borrar</button>
+                        </article>
+                    )
+
+                }) : <h3>&nbsp;No hay pelis</h3>
+            }
         </>
     )
 }
