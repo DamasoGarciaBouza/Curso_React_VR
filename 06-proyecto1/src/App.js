fcca47a2977026db646eaa107b8baf14e5ae0a95
Damaso Garcia Bouza <damasogb@gmail.com>
2024-02-26 19:57:57 +0100

    Función buscar - bug en codigo del curso

--- 06-proyecto1/src/App.js
+++ 06-proyecto1/src/App.js
@@ -30,7 +30,7 @@ function App() {
       </section>
 
       <aside className="lateral">
-        <Buscador/>
+        <Buscador listState={listState} setListState={setListState}/>
         <Crear setListState={setListState}/>
       </aside>
 
--- 06-proyecto1/src/components/Buscador.js
+++ 06-proyecto1/src/components/Buscador.js
@@ -1,12 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export const Buscador = () => {
+export const Buscador = ({ listState, setListState }) => {
+
+    // Método mostrado por Víctor solo actualiza al segundo valor, con el valor anterior
+    // Solución: llamamos a "searchMovie" con use effect
+
+    const [search, setSearchState] = useState('');
+
+    useEffect(() => {
+        searchMovie()
+    }, [search]);
+
+    const searchMovie = (e) => {
+        // Crear estado y actualizarlo
+
+        //setSearchState(e.target.value);
+
+
+        console.log("Search: " + search);
+
+
+        // Filtrar listado para buscar coincidencias
+        let found_movies = listState.filter(movie => {
+            return movie.title.toLowerCase().includes(search.toLowerCase());
+        })
+
+        if (search.length < 1) {
+            console.log("Not enough chars");
+            found_movies = JSON.parse(localStorage.getItem("movies"));
+        }
+
+        if (found_movies == 0) {
+            console.log("No movies found");            
+            found_movies = JSON.parse(localStorage.getItem("movies"));
+        }
+
+        // Actualizar estado del listado principal con lo que he logrado filtar
+        console.log(found_movies);
+        setListState(found_movies);
+    }
+
     return (
         <div className="search">
-            <h3 className="title">Buscador</h3>
+            <h3 className="title">Buscador: {search}</h3>
             <form>
-                <input type="text" />
-                <button>Buscar</button>
+                <input type="text"
+                    id='search_field'
+                    name='search'
+                    autoComplete='off'
+                    value={search}
+                    //onChange={searchMovie}
+                    onChange={(e) => setSearchState(e.target.value)}
+                />
+                <button id='search' onClick={searchMovie}>Buscar</button>
             </form>
         </div>
     )
