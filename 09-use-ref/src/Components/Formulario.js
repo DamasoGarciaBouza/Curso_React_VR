3cb093b0adfdf84283e6896bb7c478f43e7e3048
Damaso Garcia Bouza <damasogb@gmail.com>
2024-03-03 13:43:55 +0100

    Fin ejemplo 1 useRef

---
+++ 09-use-ref/src/Components/Formulario.js
@@ -0,0 +1,50 @@
+import React, { useRef } from 'react'
+
+export const Formulario = () => {
+    
+    //const nameInput = useRef("Fulano");
+    const nameInput = useRef();
+    const surnameInput = useRef();
+    const emailInput = useRef();
+
+    const miCaja = useRef();
+
+    const show = e => {
+        e.preventDefault();
+        console.log(nameInput.current.value);
+
+        console.log(miCaja);
+
+        //miCaja.current.classList.add("green");
+        // destructuracion para acceder directamente a current de miCaja
+        let { current: caja} = miCaja;
+        caja.classList.add("green");
+        caja.innerHTML = "Formulario enviado";
+
+
+    }
+
+    return (
+        <div>
+
+            <h1>Formulario</h1>
+
+            <div id="miCaja" ref={miCaja}>
+                <h2>Pruebas con Useref</h2>
+            </div>
+
+            <form onSubmit={show}>
+                <input type='text' placeholder='Nombre' ref={nameInput} />
+                <br/>
+                <input type='text' placeholder='Apellidos' ref={surnameInput} />
+                <br/>
+                <input type='email' placeholder='Email' ref={emailInput} />
+                <br/>
+                <input type='submit' submit="Enviar" />
+            </form>
+
+            <button onClick={() => nameInput.current.select()}>Empezar a rellenar formulario</button>
+
+        </div>
+    )
+}
